82a015e4f56cf54e403fde04a330f879d36febba
Neekeech24 <rogozinn135@gmail.com>
2021-07-22 14:58:32 +0300

    Testing

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ if "HEROKU" in list(os.environ.keys()):
         return "!", 200
 
 
-    @server.route("/", methods=['POST'])
+    @server.route("/", methods=['GET', 'POST'])
     def webhook():
         bot.remove_webhook()
         bot.set_webhook(
